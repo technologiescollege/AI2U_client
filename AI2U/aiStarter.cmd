8af757d7e5149a9951a575db679b33ec2783d88a
SebCanet <scanet@libreduc.cc>
2017-08-30 19:36:18 +0200

    v1

---
+++ AI2U/aiStarter.cmd
@@ -0,0 +1,9 @@
+@echo off
+break ON
+cls
+rem fichiers BAT et programme créés par Sébastien CANET
+echo 'AI2 starter...'
+cd /d %~dp0
+set ai2upath=%CD%
+set AEJSDK=.\AppEngine
+start /min .\Emulator\connector\aiWinStarter.exe
